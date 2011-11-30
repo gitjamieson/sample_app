56df5613e6f7e35e206312b6be28e0523b009f9a
bryanjamieson <jamieson.bryan@gmail.com>
2011-11-30 16:19:07 -0600

    added some test flashes to create success

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      # handle a successful save.
+      # 
       flash[:success] = "Welcome to the Sample App!"
+      flash[:error] = "this is a test of an 'error' flash label to produce a different color box then the 'success' box based on the predefined CSS for a type error verses type success for each class.  You need to look at the layout/application.erb, as this is displaying the flash and create the <div class  lines"
+      flash[:info] = "this is a test of an 'info' flash label to produce a different color box then the success box based on the predefined CSS" 
+      flash[:notice] = "look in: public/stylesheets/blueprint/screen.css. Youll find CSS values for .error, .alert, .success .. etc which define the box and color"
+
       redirect_to @user
     else
       @title = "Sign up"
