9a3d2dd88804beb3570f6ac3d7cf1fb1abc26b16
bryanjamieson <jamieson.bryan@gmail.com>
2011-12-19 09:20:34 -0600

    exercise 9.6 1

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -48,10 +48,15 @@ describe "Users" do
     describe "success" do
       it "should sign a user in and out" do
         user = Factory(:user)
-        visit signin_path
-        fill_in "Email",                      :with => user.email
-        fill_in "Password",                   :with => user.password
-        click_button
+        #======================
+        # replace below integration test with a spec helper
+        # in spec/spec_helper.rb
+        #======================
+        #visit signin_path
+        #fill_in "Email",                      :with => user.email
+        #fill_in "Password",                   :with => user.password
+        #click_button
+        integration_sign_in(user)
         controller.should be_signed_in
         click_link "Sign out"
         controller.should_not  be_signed_in
