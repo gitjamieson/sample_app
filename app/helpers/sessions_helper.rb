68b7003afb2915ef96ba3d66383942f615bda079
bryanjamieson <jamieson.bryan@gmail.com>
2011-12-23 10:39:37 -0600

    Exercise9.6.2 done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,14 @@
 module SessionsHelper
 
+  #============================
+  # for exercise 9.6, we used session
+  # instead of cookies, so cookie lines, remember token and
+  # private methods associated with cookies and remember token
+  # have been commented out
+  #============================
   def sign_in(user)
-    cookies.permanent.signed[:remember_token]= [user.id, user.salt]
+    #cookies.permanent.signed[:remember_token]= [user.id, user.salt]
+    session[:user_id] = user.id
     self.current_user = user
   end
 
@@ -10,7 +17,8 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user||=user_from_remember_token
+    #@current_user||=user_from_remember_token
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   def signed_in?
@@ -18,17 +26,18 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    #cookies.delete(:remember_token)
+    session[:user_id] = nil
     self.current_user = nil
   end
 
   private
 
-    def user_from_remember_token
-      User.authenticate_with_salt(*remember_token)
-    end
+    #def user_from_remember_token
+    #  User.authenticate_with_salt(*remember_token)
+    #end
 
-    def remember_token
-       cookies.signed[:remember_token] || [nil, nil]
-    end
+    #def remember_token
+    #   cookies.signed[:remember_token] || [nil, nil]
+    #end
 end
